fe343013c6fb7fd2ca8726860cfc70d3db55fbac
emanoeldelfino <emanoeldelfno@gmail.com>
2024-05-13 01:45:48 -0300

    feat: changing languages better, adding spanish

--- app.js
+++ app.js
@@ -1,44 +1,67 @@
-const selectors = [
-  "a[href='#about']", "a[href='#projects']", "a[href='#contact']",
-  "#about h1", "#about h2", "#projects h2", ".project-tile:nth-child(1) p",
-  ".project-tile:nth-child(2) p", ".project-tile:nth-child(3) p", ".project-tile:nth-child(4) p", ".project-tile:nth-child(5) p", ".project-tile:nth-child(6) p", "#contact h2", "footer p"
-];
+const jsonKeysWithTextSelector = {
+  "about": "a[href='#about']", 
+  "projects": "a[href='#projects']", 
+  "contact": "a[href='#contact']",
+  "about-h1": "#about h1", 
+  "about-h2": "#about h2", 
+  "projects-h2": "#projects h2", 
+  "projects-p1": ".project-tile:nth-child(1) p",
+  "projects-p2": ".project-tile:nth-child(2) p", 
+  "projects-p3": ".project-tile:nth-child(3) p", 
+  "projects-p4": ".project-tile:nth-child(4) p", 
+  "projects-p5":".project-tile:nth-child(5) p", 
+  "projects-p6": ".project-tile:nth-child(6) p", 
+  "contact-h2": "#contact h2"
+};
 
-const selectorElems = selectors.map(selector => document.querySelector(selector));
+const jsonKeysWithSelector = Object.entries(jsonKeysWithTextSelector).map(([jsonKey, selectorText]) => {
+  return {
+    jsonKey: jsonKey,
+    selector: document.querySelector(selectorText)
+  };
+});
 
-const jsonKeys = [
-  "about", "projects", "contact", "about-h1", "about-h2", "projects-h2", "projects-p1", "projects-p2", "projects-p3", "projects-p4", "projects-p5",
-  "projects-p6", "contact-h2", "footer"
-]
+const brazilFlagIcon = document.querySelector(".br-flag");
+const unitedStatesFlagIcon = document.querySelector(".us-flag");
+const spainFlagIcon = document.querySelector(".es-flag");
+const documentRoot = document.documentElement;
 
-const flagIcon = document.querySelector(".flag");
-const root = document.documentElement;
+let portugueseJson, englishJson, spanishJson, selectedLanguageJson;
 
-let lang, pt, en;
+function setLanguage(languageJson) {
+  if (selectedLanguageJson === languageJson)
+    return;
+
+  selectedLanguageJson = languageJson;
+
+  documentRoot.style.setProperty("--typewriterChars", languageJson["about-h1"].length);
+
+  jsonKeysWithSelector.forEach((item) => {
+    item.selector.innerHTML = languageJson[item.jsonKey];
+  })
+}
+
 fetch("./language/pt.json")
   .then(response => response.json())
   .then(json => {
-    pt = json;
-    lang = pt;
-})
+    portugueseJson = json;
+  })
+  .then(() => {
+    setLanguage(portugueseJson);
+  })
 
 fetch("./language/en.json")
   .then(response => response.json())
   .then(json => {
-    en = json;
+    englishJson = json;
+  })
+
+fetch("./language/es.json")
+.then(response => response.json())
+.then(json => {
+  spanishJson = json;
 })
 
-flagIcon.addEventListener("click", () => {
-  if (flagIcon.src.includes("us")) {
-    flagIcon.src = "images/br.svg";
-    lang = en;
-    root.style.setProperty("--typewriterChars", 18);
-  } else {
-    flagIcon.src = "images/us.svg";
-    lang = pt;
-    root.style.setProperty("--typewriterChars", 17);
-  }
-  selectorElems.forEach((selectorElem, index) => {
-    selectorElem.innerHTML = lang[jsonKeys[index]];
-  })
-});
+brazilFlagIcon.addEventListener("click", () => setLanguage(portugueseJson));
+unitedStatesFlagIcon.addEventListener("click", () => setLanguage(englishJson));
+spainFlagIcon.addEventListener("click", () => setLanguage(spanishJson));
